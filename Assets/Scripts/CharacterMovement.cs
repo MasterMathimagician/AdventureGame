b7bbac460d84bc89e9d6a8cbe21b3d42800cff71
Stephen Trotter <trotters@uvic.ca>
2018-02-28 18:34:09 -0800

    Started work on charcter and camera controllers
    Got the basic follow camera started and basic character controller
    started, now add more sophistication and allow for a rolling ball that
    can move to different heights and still have the camera elevated and
    “behind” the chacter

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Class: CharacterMovement
+ * Purpose: This is to control the movement of an unanimated character and produce something a little more refined than that
+ * which is seen in the online tutorials
+ * 
+ * 
+ * To add:
+ * Jumping
+ * double jumps
+ * hover
+ * variable speed movement (try to connect a dualshock 4 controller)
+ * Then move on to animated character controllers
+ * 
+*/
+
+public class CharacterMovement : MonoBehaviour {
+
+
+	public float jumpPower = 1.0f;
+	public float moveSpeed = 1.5f;
+	public float hoverFallSpeed = 0.5f;
+	public int JumpNum = 2;  
+
+	private GameObject character;
+	private Rigidbody rb = null;
+
+
+	// Use this for initialization
+	void Start () {
+		character = this.gameObject;
+		rb = GetComponent<Rigidbody> (); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+		transform.Rotate(0, x, 0);
+		transform.Translate(0, 0, z);
+	}
+}
---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+
+	public Transform lookTo;
+	public float distance = 2.7f;
+	public float height = 1.5f;
+	public float damping = 5.0f;
+	public bool smoothRotation = true;
+	public bool followBehind = true;
+	public float rotationDamp = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 wantedPosition;
+		wantedPosition = lookTo.TransformPoint (0, height, -distance);
+
+		transform.position = Vector3.Lerp (transform.position, wantedPosition, Time.deltaTime * damping);
+
+		if (smoothRotation) {
+			Quaternion wantedRotation = Quaternion.LookRotation (lookTo.position - transform.position, lookTo.up);
+			transform.rotation = Quaternion.Slerp (transform.rotation, wantedRotation, Time.deltaTime * rotationDamp);
+		} else {
+			transform.LookAt (lookTo, lookTo.up);
+		}
+	}
+}
